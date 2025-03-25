e9aa145660ef1b74360eeda9341cd3e3cac30d48
Mat-eiki <mekreiki@gmail.com>
2025-03-25 00:50:09 -0300

    v1.1.1
    Camera added, movement fixed, animation added

--- pokefake/scripts/player.gd
+++ pokefake/scripts/player.gd
@@ -1,15 +1,18 @@
+class_name Player
 extends CharacterBody2D
 
-func _process(delta: float) -> void:
-	movement(delta)
-	
+@export var movement_speed = 100
+var input: Vector2 = Vector2.ZERO
 
-func movement(x):
-	if Input.is_action_pressed("ui_right"):
-		global_position.x += 128*x
-	if Input.is_action_pressed("ui_left"):
-		global_position.x -= 128*x
-	if Input.is_action_pressed("ui_up"):
-		global_position.y -= 128*x
-	if Input.is_action_pressed("ui_down"):
-		global_position.y += 128*x
+func get_direction():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	input = input.normalized()
+
+func player_movement():
+	velocity = input * movement_speed
+
+func _physics_process(delta):
+	get_direction()
+	player_movement()
+	move_and_slide()
